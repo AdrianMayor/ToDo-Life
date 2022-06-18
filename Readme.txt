76128c85b3b6f4b894bcc41498fb9bcab44e2151
AdrianMayor <adrianmayorsantana@gmail.com>
2022-06-18 20:52:32 +0200

    commit con todo hasta el momento, JS funcional finalizado

---
+++ Readme.txt
@@ -0,0 +1,37 @@
+Lista de tareas
+
+Aplicación web que permita llevar registro de tareas
+
+pendientes.
+
+
+Descripción
+
+● La aplicación debe mostrar un formulario con una caja de texto
+donde escribir el texto de la tarea (p.ej. Comprar pan). Debe limitar
+el tamaño del texto. Un selector de prioridad de tarea (Normal o
+Importante) y un botón para añadir la tarea.
+
+● Debajo de la caja de texto debe mostrar la lista de tareas añadidas
+con la fecha que se añadieron y la prioridad.
+
+● Al lado de cada tarea debe aparecer un checkbox para marcar la
+tarea como hecha (o desmarcarla)
+
+● Las tareas marcadas como hechas deben aparecer visualmente
+tachadas.
+
+● La aplicación debe tener un botón que permita limpiar las tareas
+marcadas como hechas (borrarlas de la lista)
+
+● La lista de tareas debe guardarse en el almacenamiento del
+navegador para que se mantenga al volver a abrir la página.
+
+
+
+Recursos
+● window.localStorage1
+● JSON2
+
+1 https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+2 https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
---
+++ js/src.js
@@ -0,0 +1,150 @@
+"use strict";
+
+const forms = document.forms.taskForm;
+const inputTask = forms.elements.task;
+const taskPriority = forms.priority;
+const taskList = document.querySelector("#taskList > ul");
+// const taskButton = document.querySelector("form > button");
+
+/* inputTask.addEventListener("input", () => {
+  console.log(inputTask.value);
+}); */
+
+/* console.log(forms);
+console.log(inputTask.value);
+console.log(taskPriority.value); */
+
+printTasks();
+//console.log(taskButton);
+
+let tasks = [];
+if (JSON.parse(localStorage.getItem("tareas"))) {
+  tasks = JSON.parse(localStorage.getItem("tareas"));
+}
+//const taskJSON = JSON.stringify(tasks);
+
+/* console.log("log de tasks " + tasks.length); */
+
+class Task {
+  static description = "Esta clase define una tarea";
+
+  constructor(name, priority, date, complete) {
+    this.name = name;
+    this.priority = priority;
+    this.date = date;
+    this.complete = complete;
+  }
+}
+
+function getDate() {
+  const today = new Date();
+
+  const date =
+    today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+
+  return date;
+}
+
+function handleSubmitForm(e) {
+  //console.log(e);
+  e.preventDefault();
+
+  const complete = false;
+
+  tasks.push(
+    new Task(inputTask.value, taskPriority.value, getDate(), complete)
+  );
+
+  localStorage.setItem("tareas", JSON.stringify(tasks));
+  printTasks();
+}
+
+forms.addEventListener("submit", handleSubmitForm);
+
+function printTasks() {
+  const taskObj = JSON.parse(localStorage.getItem("tareas"));
+  const objFra = document.createDocumentFragment();
+  const button = document.createElement("button");
+
+  button.setAttribute("id", "cleanButton");
+  button.innerHTML = `Limpiar`;
+
+  if (taskObj && taskObj.length > 0) {
+    clearList("#taskList > ul");
+
+    for (let i = 0; i < taskObj.length; i++) {
+      const li = document.createElement("li");
+      li.classList.add("toDo");
+
+      if (taskObj[i].complete) {
+        li.classList.remove("toDo");
+        li.classList.add("done");
+      } else {
+        li.classList.remove("done");
+        li.classList.add("toDo");
+      }
+
+      li.setAttribute("id", i);
+      li.innerHTML = `${taskObj[i].name}. Prioridad ${taskObj[i].priority}. Añadido el dia ${taskObj[i].date}`;
+      /*       console.log(
+        "Veamos si esta cogiendo bien el obt complete",
+        taskObj[0].complete
+      ); */
+      if (taskObj[i].complete) {
+        li.style.textDecoration = "line-through";
+      }
+      objFra.append(li);
+    }
+    objFra.append(button);
+  }
+
+  /*   console.log(taskObj);
+  console.log(objFra); */
+  taskList.append(objFra);
+}
+
+function clearList(elementSelector) {
+  const element = document.querySelector(elementSelector);
+  element.innerHTML = " ";
+}
+
+function handleCrossTask() {
+  if (event.target.matches("li.toDo") || event.target.matches("li.done")) {
+    const completedTask = event.target;
+
+    tasks[completedTask.id].complete = !tasks[completedTask.id].complete;
+    console.log(event.target);
+    console.log(tasks[completedTask.id].complete);
+
+    /*  if (tasks[completedTask.id].complete) {
+      completedTask.classList.remove("toDo");
+      completedTask.classList.add("done");
+    } else {
+      completedTask.classList.remove("done");
+      completedTask.classList.add("toDo");
+    } */
+    /*     console.log("a", event.target);
+    console.log(event.target.classList); */
+
+    localStorage.setItem("tareas", JSON.stringify(tasks));
+
+    printTasks();
+  }
+}
+
+taskList.addEventListener("click", handleCrossTask);
+
+function handleDeleteButton() {
+  if (event.target.matches("ul #cleanButton")) {
+    const filteredTasks = tasks.filter((key) => key.complete === false);
+
+    tasks = filteredTasks;
+    localStorage.setItem("tareas", JSON.stringify(filteredTasks));
+    printTasks();
+    /*   console.log(filteredTasks);
+    console.log(tasks);
+    console.log(filteredTasks); */
+  }
+}
+
+taskList.addEventListener("click", handleDeleteButton);
